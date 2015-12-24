0f030a4884cd4275f482fc57fe0bac1aecce3035
Jordan Morgan <jordan@dreaminginbinary.co>
2015-12-24 15:33:40 -0600

    Initial commit

---
+++ DIBPagerDots/DIBPagerDots/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DIBPagerDots
+//
+//  Created by Jordan Morgan on 12/24/15.
+//  Copyright © 2015 Dreaming In BInary LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DIBPagerDots/DIBPagerDots/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DIBPagerDots
+//
+//  Created by Jordan Morgan on 12/24/15.
+//  Copyright © 2015 Dreaming In BInary LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
